adeb9dca945004334f4af6a6442e41dd856d1612
Google Team Member <genai-sdk-bot@google.com>
2025-12-18 07:44:48 -0800

    fix: update EmbeddingModelDiscoveryTest package statement
    PiperOrigin-RevId: 846265401

--- contrib/spring-ai/src/test/java/com/google/adk/models/springai/embeddings/EmbeddingModelDiscoveryTest.java
+++ contrib/spring-ai/src/test/java/com/google/adk/models/springai/embeddings/EmbeddingModelDiscoveryTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.adk.models.springai;
+package com.google.adk.models.springai.embeddings;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
